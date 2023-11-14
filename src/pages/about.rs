c5b2e2ff0ca87c868dd3f5001cbe64e4b6d33eed
iliags <19849002+iliags@users.noreply.github.com>
2023-11-14 14:41:59 -0700

    Update about.rs

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -9,11 +9,17 @@ impl crate::View for About {
 
         ui.separator();
 
-        ui.label("I was born in Michigan but have settled (for now) in Arizona. I spent 9 years in the United States Marine Corps, where I was deployed to Okinawa, Afghanistan, and Kuwait.");
-        ui.label("");
+        ui.vertical(|ui| {
+            ui.label("I was born in Michigan but have settled (for now) in Arizona. I spent 9 years in the United States Marine Corps, where I was deployed to Okinawa, Afghanistan, and Kuwait.");
+            ui.label("");
 
-        ui.label("My preferred tech stack leans into the bleeding edge for non-game projects; Unreal Engine is a close second.");
-        ui.label("");
+            ui.label("My introduction to web technology was in 2005 (anyone else remember applets?) which was...unpleasant. I'm really happy that Rust lets me do web things (like this site) without regular web development.");
+            ui.label("");
+
+            ui.label("I enjoy bleeding edge tech stacks, unorthodox software solutions, and Unreal Engine for game development.");
+            ui.label("");
+
+        });
 
 
     }
